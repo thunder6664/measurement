a00704e638ad9bdaaf36551fd7e1c120abfabad5
张天天 <1292534569@qq.com>
2023-04-26 00:08:22 +0800

    测试中值 均值 高斯滤波

--- cv_project01/main.cpp
+++ cv_project01/main.cpp
@@ -8,7 +8,7 @@ using namespace cv;
 using namespace std;
 int main()
 {
-	Mat src = QF_ReadImage("C:/images/measure/20.bmp");
+	Mat src = QF_ReadImage("C:/images/measure/19.bmp");
 	if (src.empty()) {
 		cout << "\033[31m invalid path of the input image \033[0m";
 		return -1;
@@ -16,10 +16,10 @@ int main()
 	Mat dst;
 	//medianBlur(src, src, 19);
 	GaussianBlur(src, dst, cv::Size(5, 5), 3, 3);
-	cvtColor(src, src, COLOR_BGR2GRAY);
+	//cvtColor(src, src, COLOR_BGR2GRAY);
 	//blur(src, src, Size(5, 5));
 	Canny(src, dst, 150, 100);
-	imwrite("C:/images/ans/gauss5_20.bmp", dst);
+	imwrite("C:/images/ans/gauss5_19.bmp", dst);
 
 	//QF_ShowImage(dst, "01", 0);
 	
